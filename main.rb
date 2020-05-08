cdb75d9829ca169de156558dce17113c6d840f14
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-05-08 14:21:03 -0700

    Started to-do list

---
+++ main.rb
@@ -0,0 +1,14 @@
+#This will hold all the `require` files to run the program, and start the game with Game.new.
+#Also my to-do list.
+
+=begin
+    
+*****MASTERMIND*****
+12 turns to guess the secret code, starting with you guessing the computer's random code.
+- the computer randomly selects the secret colors and the human player must guess them.
+    - Remember that you need to give the proper feedback on how good the guess was each turn!
+
+
+
+
+end

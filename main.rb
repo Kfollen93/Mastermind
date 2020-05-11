a03c0b14b60e2800dc523efebaca3a620efa6aad
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-05-11 11:07:33 -0700

    Updated list with files, classes, and methods notes

--- main.rb
+++ main.rb
@@ -2,12 +2,67 @@
 #Also my to-do list.
 
 =begin
+
+main.rb 
+  -Require files
+  -Start the game
+
+game.rb
+ -Game class
+
+
+create_board.rb
+-Board class
+  -Create the board method
+  def createBoard
+  end
+
+
+computer.rb
+ -Computer class
+    -Method for computer to randomize and choose colors
+    def randomize #computer selects 4 randoms numbers from 1 - 6
+      @random_nums = 4.times.map{ 1 + Random.rand(6) }.to_a 
+    end
+    -Method providing feedback on how good the guesses were each turn
+    def feedback
+    end
+
+player.rb
+ -Player class
+    -Guesses the colors
+    def player_guess
+    end
+
+instructions.rb
+  -Instructions class
+    -Text of how to play game
+    def instructions
+    end
+
+display.rb
+  -Display method
+      -displays board & displays rules
+      def display_board
+      end
+
+      def display_instructions
+      end
+
+
+
+
+
+
+
+
+
     
 *****MASTERMIND*****
 PART 1)
     - 12 turns to guess the secret code, starting with you guessing the computer's random code.
         - the computer randomly selects the secret colors and the human player must guess them.
-            - Remember that you need to give the proper feedback on how good the guess was each turn!\
+            - Remember that you need to give the proper feedback on how good the guess was each turn!
 
 PART 2)
     - Refactor my code to allow the human player to choose whether he/she wants to be
@@ -29,7 +84,7 @@ PART 2)
 
     How do I set a color to the number and display it?
         i.e. See below, this works:
-class String
+class String #### Can this go in the main file just to keep it separate and clean?
   def red;            "\e[31m#{self}\e[0m" end
   def green;          "\e[32m#{self}\e[0m" end
   def yellow;         "\e[33m#{self}\e[0m" end

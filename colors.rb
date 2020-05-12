b7acde2f2bb6cb2f33a328bd9b9597fe8079a8d6
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-05-12 13:13:05 -0700

    Updated color comment

--- colors.rb
+++ colors.rb
@@ -4,7 +4,7 @@ def code_colors(number)
   {
     '1' => "\e[41m  1 \e[0m" ' ', # red
     '2' => "\e[42m  2  \e[0m" ' ', # green
-    '3' => "\e[43m  3  \e[0m" ' ', # yellow
+    '3' => "\e[43m  3  \e[0m" ' ', # brown
     '4' => "\e[44m  4  \e[0m" ' ', # blue
     '5' => "\e[45m  5  \e[0m" ' ', # magenta
     '6' => "\e[46m  6  \e[0m" ' ' # cyan

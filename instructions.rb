012a3a10a34faf7eed2782cd46bdade69c35ed5b
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-05-11 18:43:18 -0700

    Completed instructions

--- instructions.rb
+++ instructions.rb
@@ -1,34 +1,35 @@
+# frozen_string_literal: true
+
+require_relative 'colors.rb'
+
 class Instructions
-    def instructions
-        <<~HEREDOC
-        Mastermind
+  def instructions
+    <<~HEREDOC
+     
+                                MASTERMIND
 
-        This is a 1-player game against the computer. 
-        You can choose to be the Code Maker or the Code Breaker
-        There are six different number/color combinations:
-        
-        #1.red? 2.green? etc etc
-        
-        
-        The code maker will choose four to create a 'Master Code'. For example,
-        #1.red? 2.green? etc etc
-        
-        There can be more than one of the same number/color.
-        In order to win, the Code Breaker needs to guess the 'Master Code' in 12 or less turns.
-        
-        
-        Clues:
+      There are six different numbers:
 
-        After each guess, there will be up to four clues to help crack the code.
-        # green "*" This clue means you have a correct number in the correct location.
-        # red "?" This clue means you have a correct number, but in the wrong location.
-        You will not receive any feedback for a number that is not included (which is a hint itself).
-        
-        Let's Play:
+      #{code_colors('1')} #{code_colors('2')} #{code_colors('3')} #{code_colors('4')} #{code_colors('5')} #{code_colors('6')}
 
-        Would you like to be the Code Maker or the Code Breaker?
-        Press '1' to be the Code MAKER
-        Press '2' to be the Code BREAKER
-        HEREDOC
-      end
+    
+      The Code Maker will choose four digits to create a 'Master Code' from these numbers (1 - 6).
+      There can be more than one of the same number, but only 4 digits in length.
+
+      The Code Breaker needs to guess the 'Master Code' in 12 turns or less.
+      The Code Breaker is only trying to match the numbers, the colors are purely visual.
+
+      Upon each guess there will be clues provided:
+
+      #{clue_colors("!")} Represents you have a correct number in the correct location.
+      #{clue_colors("?")} Represents you have a correct number, but in the wrong location.
+      There is no feedback for a number that is not part of the Master Code (which is a hint).
+
+      START:
+
+      Press '1' to be the Code MAKER
+      Press '2' to be the Code BREAKER
+
+    HEREDOC
+    end
     end

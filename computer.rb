61ab8c721c13b1b1dc2335d9a74e519e957444af
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-05-12 14:28:30 -0700

    Simplified code

--- computer.rb
+++ computer.rb
@@ -2,13 +2,10 @@
 
 #Human is the Code Breaker in this case.
 class Computer
-  def randomize # computer selects 4 randoms numbers from 1 - 6
-    @random_nums = 4.times.map { Random.rand(1..6) }.to_a
-  end
-
   def computer_code_set
     "...Setting Master Code.\n".each_char { |c| putc c ; sleep 0.10; $stdout.flush }
-    puts 'The Computer has set the Master Code. Try to break it.'
+    @random_nums = 4.times.map { Random.rand(1..6) }.to_a # computer selects 4 randoms numbers from 1 - 6
+    puts "The Computer has set the Master Code #{@random_nums}. Try to break it."
   end
 
   # Method providing feedback on how good the guesses were each turn

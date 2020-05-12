41c5f800de5c4b70d31b54b392dcc3a7f2f042ba
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-05-12 09:56:12 -0700

    Added Regex for until loop

--- player.rb
+++ player.rb
@@ -5,7 +5,7 @@ class Player
     1.upto(12) do |i|
       puts "Turn #{i}: Type in four numbers (1 - 6) to guess the code and press 'Enter'."
       guess = gets.chomp
-      while guess.size != 4 # This doesn't work entirely, can still enter characters.
+      until guess =~ /\A[1-6]{4}\z/
         puts 'Your guess must only be four digits, using 1 - 6.'
         guess = gets.chomp
       end

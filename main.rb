eb6bad25dde523e3007779c84ed22f8dec509b4e
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-05-12 17:32:27 -0700

    Condensed notes

--- main.rb
+++ main.rb
@@ -11,67 +11,7 @@ puts game.start_game
 player_turns = Player.new
 player_turns.player_guess
 
-# This will hold all the `require` files to run the program, and start the game with Game.new.
-# Also my to-do list.
 
-#
-# main.rb
-#   -Require files
-#   -Start the game
-#
-# game.rb
-#  -Game class
-#  def initialize
-#  end
-#
-#
-# create_board.rb
-# -Board class
-#   -Create the board method
-#   def createBoard
-#   end
-#
-#
-# computer.rb
-#  -Computer class
-#     -Method for computer to randomize and choose colors
-#     def randomize #computer selects 4 randoms numbers from 1 - 6
-#       @random_nums = 4.times.map{ 1 + Random.rand(6) }.to_a
-#     end
-#     -Method providing feedback on how good the guesses were each turn
-#     def feedback
-#     end
-#
-# player.rb
-#  -Player class
-#     -Guesses the colors
-#     def player_guess
-#     end
-#
-# instructions.rb
-#   -Instructions class
-#     -Text of how to play game
-#     def instructions
-#     end
-#
-# display.rb
-#   -Display method
-#       -displays board & displays rules
-#       def display_board
-#       end
-#
-#       def display_instructions
-#       end
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
 # *****MASTERMIND*****
 # PART 1)
 #     - 12 turns to guess the secret code, starting with you guessing the computer's random code.
@@ -86,39 +26,4 @@ player_turns.player_guess
 #              -Next, add a little bit more intelligence to the computer player so that,
 #                if the computer has guessed the right color but the wrong position,
 #                  its next guess will need to include that color somewhere
-#
-#
-#
-#
-#
-# 1. Would need a Computer Class.
-#     - It would have an array with six numbers to randomize and hold, but can only choose four of the six.
-#     i.e. random_nums = 4.times.map{ 1 + Random.rand(6) }.to_a
-#          print random_nums
-#
-#     How do I set a color to the number and display it?
-#         i.e. See below, this works:
-# class String #### Can this go in the main file just to keep it separate and clean?
-#   def red;            "\e[31m#{self}\e[0m" end
-#   def green;          "\e[32m#{self}\e[0m" end
-#   def yellow;         "\e[33m#{self}\e[0m" end
-#   def blue;           "\e[34m#{self}\e[0m" end
-#   def magenta;        "\e[35m#{self}\e[0m" end
-#   def cyan;           "\e[36m#{self}\e[0m" end
-# end
-#
-# one = "1".red
-# two = "2".green
-# three = "3".yellow
-# four = "4".blue
-# five = "5".magenta
-# six = "6".cyan
-#
-# puts "Enter number you want"
-# guess = gets.chomp.to_s
-# if guess == "3"  #but this wouldn't work because there's too many combinations for if statements
-#   puts three
-# end
-#
-#
-#
+

3ec4926ca57dfe4e584d9f71157cd64413d2d2db
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-05-14 09:20:59 -0700

    Added clues method

--- clues.rb
+++ clues.rb
@@ -1,20 +1,11 @@
-=begin
-I need to convert guess = gets.chomp to an array in player.rb
-Then I need to change back @@random_nums to leave that as an array (how it originally was) in computer.rb
-
 def clues
-    guess = [1, 2, 3, 4]
-    master = [4, 2, 3, 6]
-
     matches = guess.map.with_index do |value, index|
-    if master[index] == value
+    if @@random_nums[index] == value
         "!"
-    elsif master.include?(value)
+    elsif @@random_nums.include?(value)
         "?"
     else
         nil
     end
     end
 end
-
-=end

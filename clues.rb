1a2f5f8ecb1e38ac066c079dadd68074fe09cf19
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-05-14 09:42:58 -0700

    Added colors to the clues

--- clues.rb
+++ clues.rb
@@ -2,9 +2,9 @@ class Clues
     def self.feedback
         matches = $guess.map.with_index do |value, index|
         if $random_nums[index] == value
-            "!"
+            print "#{clue_colors("!")}"
         elsif $random_nums.include?(value)
-            "?"
+            print "#{clue_colors("?")}"
         else
             nil
         end

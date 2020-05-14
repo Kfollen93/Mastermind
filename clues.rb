33ffe9a3dfc61bd3d3cefa3458a44a491aefeec8
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-05-14 09:22:52 -0700

    Added Clues class and changed clues method to feedback

--- clues.rb
+++ clues.rb
@@ -1,11 +1,13 @@
-def clues
-    matches = guess.map.with_index do |value, index|
-    if @@random_nums[index] == value
-        "!"
-    elsif @@random_nums.include?(value)
-        "?"
-    else
-        nil
-    end
+class Clues
+    def feedback
+        matches = guess.map.with_index do |value, index|
+        if @@random_nums[index] == value
+            "!"
+        elsif @@random_nums.include?(value)
+            "?"
+        else
+            nil
+        end
+        end
     end
 end

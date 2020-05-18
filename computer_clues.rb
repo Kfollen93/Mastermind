10e08732728106507116498472f52d3b7d435e8e
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-05-18 10:51:24 -0700

    Added player_master_array var

--- computer_clues.rb
+++ computer_clues.rb
@@ -1,13 +1,13 @@
 
 class ComputerClues
     def self.exact_matches
-        @temp_master = $master_code.clone #This will need new name and physical input from human.
+        @temp_player_master = $player_master_array.clone #This will need new name and physical input from human.
         #@temp_guess = $guess_array.clone  This will need to match up with the computer's input.
         exact = 0
-        @temp_master.each_with_index do | value, index |
+        @temp_player_master.each_with_index do | value, index |
           if value == @temp_guess[index]
             exact += 1 
-            @temp_master[index] = "!"
+            @temp_player_master[index] = "!"
             @temp_guess[index]  = "!"
           end
         end
@@ -17,10 +17,10 @@ class ComputerClues
       def self.correct_numbers
         correct = 0
         @temp_guess.each_with_index do | value, index |
-          if @temp_guess[index] != "!" && @temp_master.include?(@temp_guess[index])
+          if @temp_guess[index] != "!" && @temp_player_master.include?(@temp_guess[index])
             correct += 1
-            remove = @temp_master.find_index(@temp_guess[index])
-            @temp_master[remove] = "?"
+            remove = @temp_player_master.find_index(@temp_guess[index])
+            @temp_player_master[remove] = "?"
             @temp_guess[index]  = "?"
           end
         end

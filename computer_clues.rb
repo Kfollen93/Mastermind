132cc5c4d7c9ae9155cf8052031352c991dfafb6
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-05-18 11:03:11 -0700

    Refactored clues to fit for Computer variables

--- computer_clues.rb
+++ computer_clues.rb
@@ -1,14 +1,14 @@
 
 class ComputerClues
     def self.exact_matches
-        @temp_player_master = $player_master_array.clone #This will need new name and physical input from human.
-        #@temp_guess = $guess_array.clone  This will need to match up with the computer's input.
+        @temp_player_master = $player_master_array.clone
+        @temp_computer_guess = $guess.clone
         exact = 0
         @temp_player_master.each_with_index do | value, index |
-          if value == @temp_guess[index]
+          if value == @temp_computer_guess[index]
             exact += 1 
             @temp_player_master[index] = "!"
-            @temp_guess[index]  = "!"
+            @temp_computer_guess[index]  = "!"
           end
         end
         exact.times{print clue_colors("!")}
@@ -16,12 +16,12 @@ class ComputerClues
     
       def self.correct_numbers
         correct = 0
-        @temp_guess.each_with_index do | value, index |
-          if @temp_guess[index] != "!" && @temp_player_master.include?(@temp_guess[index])
+        @temp_computer_guess.each_with_index do | value, index |
+          if @temp_computer_guess[index] != "!" && @temp_player_master.include?(@temp_computer_guess[index])
             correct += 1
-            remove = @temp_player_master.find_index(@temp_guess[index])
+            remove = @temp_player_master.find_index(@temp_computer_guess[index])
             @temp_player_master[remove] = "?"
-            @temp_guess[index]  = "?"
+            @temp_computer_guess[index]  = "?"
           end
         end
         correct.times{print clue_colors("?")}

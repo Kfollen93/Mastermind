47a1863306fd988056cd41b3d32a8cb74ff9b289
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-05-19 09:06:27 -0700

    Removed puts on line 13

--- main.rb
+++ main.rb
@@ -10,7 +10,7 @@ require_relative 'computer_clues.rb'
 
 game = Instructions.new
 puts game.instructions
-puts game.start_game
+game.start_game
 player_turns = Player.new
 pc_logic = ComputerLogic.new
 $start == "1" ? pc_logic.computer_guess : player_turns.player_guess

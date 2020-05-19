a1064b4c1f785b74fa104826da08cd711d5f40c1
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-05-19 16:49:46 -0700

    Removed empty spaces

--- colors.rb
+++ colors.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 def clue_colors(number)
-  {
-    '?' => "\e[41m  ?  \e[0m" ' ', # red
-    '!' => "\e[42m  !  \e[0m" ' ', # green
-  }[number]
-  end
+	{
+		'?' => "\e[41m  ?  \e[0m" ' ', # red
+		'!' => "\e[42m  !  \e[0m" ' ', # green
+	}[number]

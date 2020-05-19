833a06d23662d7f4ad65f6a92a459b281e1ca064
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-05-19 16:34:40 -0700

    Removed unused comments

--- main.rb
+++ main.rb
@@ -14,23 +14,3 @@ game.start_game
 player_turns = Player.new
 pc_logic = ComputerLogic.new
 $start == "1" ? pc_logic.computer_guess : player_turns.player_guess
-
-
-
-
-
-# *****MASTERMIND*****
-# PART 1)
-#     - 12 turns to guess the secret code, starting with you guessing the computer's random code.
-#         - the computer randomly selects the secret colors and the human player must guess them.
-#             - Remember that you need to give the proper feedback on how good the guess was each turn!
-#
-# PART 2)
-#     - Refactor my code to allow the human player to choose whether he/she wants to be
-#      the creator of the secret code or the guesser.
-#         - Build it out so that the computer will guess if you decide to choose your own secret colors.
-#             Start by having the computer guess randomly (but keeping the ones that match exactly).
-#              -Next, add a little bit more intelligence to the computer player so that,
-#                if the computer has guessed the right color but the wrong position,
-#                  its next guess will need to include that color somewhere
-

33e9c9384505c19657fe8a1836ce2ee8cee457f5
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-05-20 09:52:15 -0700

    Format

--- computer_clues.rb
+++ computer_clues.rb
@@ -4,7 +4,7 @@ class ComputerClues
 		@temp_player_master = $player_master_array.clone
 		@temp_computer_guess = $guess.clone
 		exact = 0
-		@temp_player_master.each_with_index do | value, index |
+		@temp_player_master.each_with_index do |value, index|
 			if value == @temp_computer_guess[index]
 				exact += 1 
 				@temp_player_master[index] = "!"
@@ -15,7 +15,7 @@ class ComputerClues
 	end
 		def self.correct_numbers
 		correct = 0
-		@temp_computer_guess.each_with_index do | value, index |
+		@temp_computer_guess.each_with_index do |value, index|
 			if @temp_computer_guess[index] != "!" && @temp_player_master.include?(@temp_computer_guess[index])
 				correct += 1
 				remove = @temp_player_master.find_index(@temp_computer_guess[index])
